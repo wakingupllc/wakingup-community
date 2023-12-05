5da7dd8b80ee7f24c4313dbdb507f16d49ff87ae
Michael Keenan <michael.keenan@gmail.com>
2023-12-05 13:54:32 -0800

    Disable login test rather than comment it out

--- cypress/e2e/set_username.cy.js
+++ cypress/e2e/set_username.cy.js
@@ -6,18 +6,17 @@ describe('Basic Login and Signup', function() {
     cy.fixture('users/testUserUnsetUsername').as('testUserUnsetUsername');
   });
 
-  // Waking Up has its own signup system, but this is left commented out here as a reminder to
-  // maybe write some Cypress tests for it.
-
-  // it('Prompts users to set their display name after signup.', function() {
-  //   const newDisplayname = 'New User 123123';
-  //   cy.loginAs(this.testUserUnsetUsername);
-  //   cy.visit('/');
-  //   cy.contains('Please choose a username').should('exist');
-  //   cy.get('input[type="text"]').type(newDisplayname);
-  //   cy.get('.NewUserCompleteProfile-submitButtonSection > button').click();
-  //   // This is not a great test, but it should work -
-  //   // the notifications icon should only appear after the user has set their username.
-  //   cy.get(`button.NotificationsMenuButton-buttonClosed`).should('exist');
-  // });
+  // Waking Up has its own signup system so we're skipping this test
+  // TODO: writing a test for the Waking Up signup system.
+  it.skip('Prompts users to set their display name after signup.', function() {
+    const newDisplayname = 'New User 123123';
+    cy.loginAs(this.testUserUnsetUsername);
+    cy.visit('/');
+    cy.contains('Please choose a username').should('exist');
+    cy.get('input[type="text"]').type(newDisplayname);
+    cy.get('.NewUserCompleteProfile-submitButtonSection > button').click();
+    // This is not a great test, but it should work -
+    // the notifications icon should only appear after the user has set their username.
+    cy.get(`button.NotificationsMenuButton-buttonClosed`).should('exist');
+  });
 })
