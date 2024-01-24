6b5522e166cc72b2e383e79bc127540306079c98
Michael Keenan <michael.keenan@gmail.com>
2024-01-24 13:41:16 -0800

    Tidy code

--- packages/lesswrong/components/comments/CommentsViews.tsx
+++ packages/lesswrong/components/comments/CommentsViews.tsx
@@ -10,7 +10,7 @@ import type { Option } from '../common/InlineSelect';
 import { getCommentViewOptions } from '../../lib/commentViewOptions';
 import { useNavigate } from '../../lib/reactRouterWrapper';
 
-const CommentsViews = ({post, setRestoreScrollPos}: {post?: PostsDetails, setRestoreScrollPos?: Function}) => {
+const CommentsViews = ({post, setRestoreScrollPos}: {post?: PostsDetails, setRestoreScrollPos?: (pos: number) => void}) => {
   const currentUser = useCurrentUser();
   const navigate = useNavigate();
   const location = useLocation();
@@ -22,8 +22,8 @@ const CommentsViews = ({post, setRestoreScrollPos}: {post?: PostsDetails, setRes
     const view = opt.value
     const { query } = location;
     const currentQuery = isEmpty(query) ? {view: 'postCommentsTop'} : query
-    const { commentId = undefined, ...newQuery } =  {...currentQuery, view: view, postId: post ? post._id : undefined}
-    if (setRestoreScrollPos) setRestoreScrollPos(window.scrollY);
+    const newQuery =  {...currentQuery, view: view, postId: post ? post._id : undefined, commentId: undefined}
+    setRestoreScrollPos?.(window.scrollY);
     navigate({...location.location, search: `?${qs.stringify(newQuery)}`})
   };
 
